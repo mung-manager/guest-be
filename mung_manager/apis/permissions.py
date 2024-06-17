37e8a9032b87e798f03c08054fe62405688a29be
DongwookKim0823 <dwkim0823@naver.com>
2024-06-17 13:58:42 +0900

    Feat : 게스트 권한 확인 기능 추가 #5

--- mung_manager/apis/permissions.py
+++ mung_manager/apis/permissions.py
@@ -2,25 +2,25 @@ from rest_framework import permissions
 from rest_framework.request import Request
 from rest_framework.views import APIView
 
-# from mung_manager.authentication.enums import AuthGroup
+from mung_manager.authentication.enums import AuthGroup
 
 
-class IsPartnerPermission(permissions.BasePermission):
+class IsGuestPermission(permissions.BasePermission):
     def has_permission(self, request: Request, view: APIView):
-        """이 함수는 기본적으로 사장님 API에 적용되며, 유저의 권한이 사장님인지 확인합니다.
+        """이 함수는 기본적으로 게스트 API에 적용되며, 유저의 권한이 게스트인지 확인합니다.
 
         Args:
             request (Request): Request 객체
             view (APIView): APIView 객체
 
         Returns:
-            bool: 유저의 권한이 사장님이면 True, 아니면 False를 반환
+            bool: 유저의 권한이 게스트이면 True, 아니면 False를 반환
         """
-        # try:
-        #     if request.user.groups.first().id == AuthGroup.PARTNER.value:  # type: ignore
-        #         return True
-        #
-        #     return False
-        #
-        # except Exception:
-        #     return False
+        try:
+            if request.user.groups.first().id == AuthGroup.GUEST.value:  # type: ignore
+                return True
+
+            return False
+
+        except Exception:
+            return False
