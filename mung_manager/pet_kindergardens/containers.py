2e64a6b0bfd0513afbdf1d6fe8797caf4bac42d6
DongwookKim0823 <dwkim0823@naver.com>
2024-06-24 19:16:28 +0900

    Docs : PetKindergardenContainer docstring 수정

--- mung_manager/pet_kindergardens/containers.py
+++ mung_manager/pet_kindergardens/containers.py
@@ -15,6 +15,7 @@ class PetKindergardenContainer(containers.DeclarativeContainer):
 
     Attributes:
         pet_kindergarden_selector: 반려동물 유치원 셀렉터
+        customer_selector: 고객 셀렉터
         pet_kindergarden_service: 반려동물 유치원 서비스
 
     """
