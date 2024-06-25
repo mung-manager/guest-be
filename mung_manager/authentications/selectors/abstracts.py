21a4d1434907a3dbe8920d826ef9df2e8475a8e8
DongwookKim0823 <dwkim0823@naver.com>
2024-06-26 02:25:47 +0900

    Feat : 파트너 유저가 로그인 시 게스트 그룹에 추가

--- mung_manager/authentications/selectors/abstracts.py
+++ mung_manager/authentications/selectors/abstracts.py
@@ -9,3 +9,7 @@ class AbstractUserSelector(ABC):
     @abstractmethod
     def get_by_social_id(self, social_id: str) -> Optional[User]:
         raise NotImplementedException()
+
+    @abstractmethod
+    def exists_by_user_id_and_group_id(self, user_id: int, partner_group_id: int, guest_group_id: int) -> bool:
+        raise NotImplementedException()
--- mung_manager/authentications/selectors/users.py
+++ mung_manager/authentications/selectors/users.py
@@ -5,10 +5,13 @@ from mung_manager.authentications.selectors.abstracts import AbstractUserSelecto
 
 
 class UserSelector(AbstractUserSelector):
-    """이 클래스는 유저를 DB에 PULL하는 비즈니스 로직을 담당합니다."""
+    """
+    이 클래스는 유저를 DB에 PULL하는 비즈니스 로직을 담당합니다.
+    """
 
     def get_by_social_id(self, social_id: str) -> Optional[User]:
-        """이 함수는 소셜 아이디로 유저를 조회합니다.
+        """
+        이 함수는 소셜 아이디로 유저를 조회합니다.
 
         Args:
             social_id (str): 소셜 아이디
@@ -20,3 +23,17 @@ class UserSelector(AbstractUserSelector):
             return User.objects.filter(social_id=social_id).get()
         except User.DoesNotExist:
             return None
+
+    def exists_by_user_id_and_group_id(self, user_id: int, partner_group_id: int, guest_group_id: int) -> bool:
+        """
+        이 함수는 유저 아이디와 그룹 아이디로 해당 유저가 게스트가 아니면서 파트너인지 확인합니다.
+
+        Args:
+            user_id (int): 유저 아이디
+            partner_group_id (int): 파트너 그룹 아이디
+            guest_group_id (int): 게스트 그룹 아이디
+
+        Returns:
+            bool: 파트너 그룹에 속해 있으면 True, 아니면 False를 반환
+        """
+        return User.objects.filter(id=user_id, groups__id=partner_group_id).exclude(groups__id=guest_group_id).exists()
