aac3c58bae0a708432420910b47d7bed6c88c861
DongwookKim0823 <dwkim0823@naver.com>
2024-06-30 14:53:06 +0900

    Refact : OutputSerializer 상속 클래스 수정

--- mung_manager/reservations/apis/apis.py
+++ mung_manager/reservations/apis/apis.py
@@ -1,9 +1,10 @@
-from rest_framework import serializers, status
+from rest_framework import status
 from rest_framework.request import Request
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
 from mung_manager.apis.mixins import APIAuthWithPetKindergardenAccessMixin
+from mung_manager.commons.base.serializers import BaseSerializer
 from mung_manager.commons.constants import SYSTEM_CODE
 from mung_manager.commons.selectors import get_object_or_not_found
 from mung_manager.customers.containers import CustomerContainer
@@ -15,7 +16,7 @@ from mung_manager.reservations.containers import ReservationContainer
 
 
 class CustomerPetAndTicketListAPI(APIAuthWithPetKindergardenAccessMixin, APIView):
-    class OutputSerializer(serializers.Serializer):
+    class OutputSerializer(BaseSerializer):
         customer_pets = CustomerPetSerializer(many=True, label="고객 반려동물 목록")
         customer_tickets = CustomerTicketSerializer(many=True, label="고객 티켓 목록")
 
