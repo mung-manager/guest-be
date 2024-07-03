b10b8cd1b526346792f6bf20ff593b2bfe82a1c5
DongwookKim0823 <ehddnr7355@gmail.com>
2024-07-03 18:31:19 +0900

    Feat #25 - 유치원 요약 정보 조회 API 구현 (#26)
    - 유치원 요약 정보 조회 기능 구현
    - PetKindergardenSelector 클래스의 get_queryset_by_user 메서드 반환 타입 수정
    
    Close #25

--- mung_manager/pet_kindergardens/selectors/abstracts.py
+++ mung_manager/pet_kindergardens/selectors/abstracts.py
@@ -1,16 +1,27 @@
 from abc import ABC, abstractmethod
 from typing import Any
 
-from django.db.models.query import QuerySet
+from django.db.models import QuerySet
+from django_stubs_ext import ValuesQuerySet, WithAnnotations
 
 from mung_manager.errors.exceptions import NotImplementedException
+from mung_manager.pet_kindergardens.models import PetKindergarden
+from mung_manager.pet_kindergardens.selectors.pet_kindergardens import (
+    info_for_full_address,
+)
+from mung_manager.pet_kindergardens.types import info_for_summary
 
 
 class AbstractPetKindergardenSelector(ABC):
 
     @abstractmethod
     def get_queryset_by_user(
-        self,
-        user,
-    ) -> QuerySet[Any]:
+        self, user
+    ) -> QuerySet[WithAnnotations[PetKindergarden, info_for_full_address], dict[str, Any]]:
+        raise NotImplementedException()
+
+    @abstractmethod
+    def get_by_pet_kindergarden_id_for_summary_info(
+        self, pet_kindergarden_id: int
+    ) -> ValuesQuerySet[PetKindergarden, info_for_summary]:
         raise NotImplementedException()
