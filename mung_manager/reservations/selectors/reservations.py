0be0be801f1ec71686a36656fcb07e0899788ce6
DongwookKim0823 <dwkim0823@naver.com>
2024-07-06 03:07:19 +0900

    Refact : get_queryset_by_customer_and_pet_kindergarden 메서드 개선

--- mung_manager/reservations/selectors/reservations.py
+++ mung_manager/reservations/selectors/reservations.py
@@ -48,44 +48,35 @@ class ReservationSelector(AbstractReservationSelector):
         # 각 예약을 그룹화
         grouped_reservations = defaultdict(list)
         for reservation in reservations:
-            if reservation.customer_ticket.ticket.ticket_type == TicketType.HOTEL.value:
+            reservation_id = reservation.id
+            if reservation.customer_ticket.ticket.ticket_type == TicketType.HOTEL.value and reservation.is_extented:
                 root_reservation = self.find_root(reservation)
-                grouped_reservations[root_reservation.id].append(reservation)
-            else:
-                grouped_reservations[reservation.id].append(reservation)
+                reservation_id = root_reservation.id
+            grouped_reservations[reservation_id].append(reservation)
 
         # 그룹화된 예약 정보를 딕셔너리로 변환
         for group in grouped_reservations.values():
             if len(group) == 1 and group[0].customer_ticket.ticket.ticket_type != TicketType.HOTEL.value:
                 reservation = group[0]
                 ticket_type_value = reservation.customer_ticket.ticket.ticket_type
-                customer_pet_name = reservation.customer_pet.name
-                start_at = reservation.reserved_at
-                end_at = reservation.end_at
-
-                ticket_type = (
-                    TicketType.TIME.value if ticket_type_value == TicketType.TIME.value else TicketType.ALL_DAY.value
-                )
-                reservation_list.append(
-                    {
-                        "ticket_type": ticket_type,
-                        "start_at": start_at,
-                        "end_at": end_at,
-                        "customer_pet_name": customer_pet_name,
-                    }
-                )
+                ticket_type = TicketType.TIME.value if ticket_type_value == TicketType.TIME.value else TicketType.ALL_DAY.value
+                reservation_data = {
+                    "ticket_type": ticket_type,
+                    "start_at": reservation.reserved_at,
+                    "end_at": reservation.end_at,
+                    "customer_pet_name": reservation.customer_pet.name,
+                }
             else:
                 ticket_type = TicketType.HOTEL.value
-                start_time = min(res.reserved_at for res in group)  # type: ignore
-                end_time = max(res.end_at for res in group)  # type: ignore
-                customer_pet_name = group[0].customer_pet.name
-                reservation_list.append(
-                    {
-                        "ticket_type": ticket_type,
-                        "start_at": start_time,
-                        "end_at": end_time,
-                        "customer_pet_name": customer_pet_name,
-                    }
-                )
+                start_time = min(res.reserved_at for res in group)
+                end_time = max(res.end_at for res in group)
+                reservation_data = {
+                    "ticket_type": ticket_type,
+                    "start_at": start_time,
+                    "end_at": end_time,
+                    "customer_pet_name": group[0].customer_pet.name,
+                }
+
+            reservation_list.append(reservation_data)
 
         return reservation_list
