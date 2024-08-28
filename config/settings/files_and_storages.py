6067e7c95827cb1997e06989d352a3c1437eabad
DongwookKim0823 <dwkim0823@naver.com>
2024-08-28 17:45:55 +0900

    Fix : 정적 파일 설정 변경
    - 정적 파일 설정 수정
    - django-storages 버전 업데이트

--- config/settings/files_and_storages.py
+++ config/settings/files_and_storages.py
@@ -3,8 +3,6 @@ from config.env import env, BASE_DIR
 
 FILE_MAX_SIZE = env.int("FILE_MAX_SIZE", default=10485760)  # 10 MiB
 
-DEFAULT_FILE_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
-
 AWS_S3_ACCESS_KEY_ID = env("AWS_S3_ACCESS_KEY_ID")
 AWS_S3_SECRET_ACCESS_KEY = env("AWS_S3_SECRET_ACCESS_KEY")
 AWS_S3_REGION_NAME = env.str("AWS_S3_REGION_NAME")
@@ -18,10 +16,19 @@ AWS_S3_OBJECT_PARAMETERS = {"CacheControl": "max-age=86400"}
 # ==================================================================== #
 USE_S3 = env.bool("USE_S3", default=False)
 
+STORAGES = {
+    "default": {
+        "BACKEND": "storages.backends.s3.S3Storage",
+    },
+    "staticfiles": {
+        "BACKEND": "storages.backends.s3.S3StaticStorage",
+    },
+}
+
+STATIC_ROOT = os.path.join(BASE_DIR, "static")
+
 if USE_S3:
     AWS_LOCATION = "guest/static"
     STATIC_URL = f"{AWS_S3_URL}/{AWS_LOCATION}/"
-    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 else:
-    STATIC_ROOT = os.path.join(BASE_DIR, "static")
     STATIC_URL = "/guest/static/"
