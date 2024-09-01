fa2abf9e5475df4d919e3a26bc64c6452208c371
DongwookKim0823 <dwkim0823@naver.com>
2024-09-02 01:26:34 +0900

    Feat : 보호자 활성 상태 여부 조회 기능

--- mung_manager/customers/apis/api_managers.py
+++ mung_manager/customers/apis/api_managers.py
@@ -4,6 +4,7 @@ from rest_framework import status
 
 from mung_manager.commons.base.api_managers import BaseAPIManager
 from mung_manager.customers.apis.apis import (
+    CustomerActiveStatusAPI,
     CustomerCreateReservationAPI,
     CustomerReservationCancelAPI,
     CustomerReservationDetailListAPI,
@@ -360,3 +361,50 @@ class CustomerReservationCancelAPIManager(BaseAPIManager):
     )
     def delete(self, request, *args, **kwargs):
         return self.VIEWS_BY_METHOD["DELETE"]()(request, *args, **kwargs)
+
+
+class CustomerActiveStatusAPIManager(BaseAPIManager):
+    VIEWS_BY_METHOD = {
+        "GET": CustomerActiveStatusAPI.as_view,
+    }
+
+    @extend_schema(
+        tags=["고객"],
+        summary="고객의 활성화 여부 조회",
+        description="""
+        Rogic
+            - 고객의 활성화 여부 조회 API 입니다.
+        """,
+        responses={
+            status.HTTP_200_OK: VIEWS_BY_METHOD["GET"]().cls.OutputSerializer,
+            status.HTTP_401_UNAUTHORIZED: OpenApiResponse(
+                response=OpenApiTypes.OBJECT,
+                examples=[
+                    ErrorAuthenticationFailedSchema,
+                    ErrorNotAuthenticatedSchema,
+                    ErrorInvalidTokenSchema,
+                    ErrorAuthorizationHeaderSchema,
+                    ErrorAuthenticationPasswordChangedSchema,
+                    ErrorAuthenticationUserDeletedSchema,
+                    ErrorAuthenticationUserNotFoundSchema,
+                    ErrorTokenIdentificationSchema,
+                ],
+            ),
+            status.HTTP_403_FORBIDDEN: OpenApiResponse(
+                response=OpenApiTypes.OBJECT,
+                examples=[ErrorPermissionDeniedSchema],
+            ),
+            status.HTTP_404_NOT_FOUND: OpenApiResponse(
+                response=OpenApiTypes.OBJECT,
+                examples=[
+                    ErrorPetKindergardenNotFoundSchema,
+                    ErrorCustomerNotFoundSchema,
+                ],
+            ),
+            status.HTTP_500_INTERNAL_SERVER_ERROR: OpenApiResponse(
+                response=OpenApiTypes.OBJECT, examples=[ErrorUnknownServerSchema]
+            ),
+        },
+    )
+    def get(self, request, *args, **kwargs):
+        return self.VIEWS_BY_METHOD["GET"]()(request, *args, **kwargs)
