b0a98a3919b385bd6af3bd3de49a0689bcaffa01
DongwookKim0823 <dwkim0823@naver.com>
2024-09-07 16:12:33 +0900

    Chore : sqs 연결 관련 Celery 설정 추가

--- config/settings/celery.py
+++ config/settings/celery.py
@@ -1,13 +1,35 @@
+from kombu.utils.url import safequote
+
+
 from config.env import env
 
 
-CELERY_BROKER_URL = env("CELERY_BROKER_URL", default="redis://localhost:6379/0")
-CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+# Credentials
+AWS_ACCESS_KEY_ID = env("AWS_SQS_ACCESS_KEY_ID", default=None)
+AWS_SECRET_ACCESS_KEY = env("AWS_SQS_SECRET_ACCESS_KEY", default=None)
+AWS_REGION = env("AWS_SQS_REGION_NAME", default=None)
+aws_access_key_id = safequote(AWS_ACCESS_KEY_ID)
+aws_secret_access_key = safequote(AWS_SECRET_ACCESS_KEY)
 
-CELERY_RESULT_BACKEND = "django-db"
 
+# Celery
+CELERY_DEFAULT_QUEUE = "sqs"
+CELERY_ACCEPT_CONTENT = ["application/json"]
+CELERY_TASK_SERIALIZER = "json"
+CELERY_RESULT_SERIALIZER = "json"
 CELERY_TIMEZONE = "Asia/Seoul"
-
 CELERY_TASK_SOFT_TIME_LIMIT = 20  # seconds
 CELERY_TASK_TIME_LIMIT = 30  # seconds
 CELERY_TASK_MAX_RETRIES = 3
+CELERY_TASK_DEFAULT_QUEUE = "sqs"
+CELERY_RESULT_BACKEND = "django-db"
+
+
+CELERY_BROKER_URL = f"sqs://{aws_access_key_id}:{aws_secret_access_key}@"
+CELERY_BROKER_CONNECTION_RETRY_ON_STARTUP = True
+CELERY_BROKER_TRANSPORT_OPTIONS = {
+    "region": AWS_REGION,
+    "visibility_timeout": 3600,
+    "polling_interval": 60,
+    "CELERYD_PREFETCH_MULTIPLIER": 0,
+}
